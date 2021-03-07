ad418342559105e3917d5d4a4711280c0734f4b1
skramm <sebastien.kramm@univ-rouen.fr>
2021-03-07 20:16:43 +0100

    WIP

--- temp1.cpp
+++ temp1.cpp
@@ -1,6 +1,6 @@
 
 //#include <boost/histogram.hpp>
-
+#define DEBUG
 #include "dtcpp.h"
 #include <iostream>
 #include <map>
@@ -35,7 +35,6 @@ using namespace std;
 using namespace dtcpp;
 
 //using namespace boost::histogram;
-//using PairAtvalClass = std::pair<float,ClassVal>;
 using PairAtvalClass = std::pair<float,ClassVal>;
 
 struct MyHistogram;
@@ -44,9 +43,7 @@ struct MyHistogram;
 struct MyBin
 {
 	friend struct MyHistogram;
-//	static const MyHistogram* p_histo;
 
-//	size_t                    _counter = 0u;
 	std::map<ClassVal,size_t> _mClassCounter;
 	float                     _startValue;
 	std::vector<size_t>       _vIdxPt;      ///< indexes of the points in original dataset
@@ -58,7 +55,8 @@ struct MyBin
 			return true;
 		return false;
 	}
-	size_t size() const { return _vIdxPt.size(); }
+	size_t size()      const { return _vIdxPt.size(); }
+	size_t nbClasses() const { return _mClassCounter.size(); }
 };
 
 
@@ -66,69 +64,101 @@ struct MyBin
 /// Variable bin-size histogram
 struct MyHistogram
 {
-	const std::vector<PairAtvalClass>* p_src = 0;  ///< pointer on source data
-	std::list<MyBin> _lBins;
-	double _step;
-	double _vmin;
+	private:
+		const std::vector<PairAtvalClass>* p_src = 0;  ///< pointer on source data
+		std::list<MyBin> _lBins;
+		double _step;
+		double _vmin;
 
-	MyHistogram( const std::vector<PairAtvalClass>& src, size_t nbBins, float vmin, float vmax )
-		: p_src( &src )
-		, _vmin(vmin)
-	{
-		_step = (vmax - vmin) / nbBins;
-		_lBins.resize( nbBins );
-
-		int i = 0;
-		for( auto& b: _lBins )
-			b._startValue = vmin + i * _step;
-	}
-
-	const auto begin() const { return _lBins.begin(); }
-	const auto end()   const { return _lBins.end();   }
-	auto begin() { return _lBins.begin(); }
-	auto end()   { return _lBins.end();   }
-
-	PairAtvalClass getPoint(size_t idx) const
-	{
-		assert( p_src );
-		return p_src->at(idx);
-	}
-	void splitBin( decltype( _lBins.begin() ) );
-
-	void assignToBin( const PairAtvalClass& pac, size_t idx )
-	{
-		auto val = (pac.first - _vmin) / _step;
-		cout <<"b=" << val << " bin=" << std::trunc( val ) << '\n';
-
-		bool match = false;
-		auto it_prev = _lBins.begin();
-
-		for( auto it=_lBins.begin(); it!=_lBins.end(); it++ )    // iterate the bins, except the last one
+	public:
+		MyHistogram( const std::vector<PairAtvalClass>& src, size_t nbBins, float vmin, float vmax )
+			: p_src( &src )
+			, _vmin(vmin)
 		{
-			match = false;
-			auto& bin = *it;
-			if( pac.first >= bin._startValue )
-				match = true;
+			_step = (vmax - vmin) / nbBins;
+//			COUT << "STEP=" << _step << '\n';
+			_lBins.resize( nbBins );
 
-			if( match == false )
-			{
-				auto& bin_prev = *it_prev;
-				bin_prev._vIdxPt.push_back( idx );
-				bin_prev._mClassCounter[pac.second]++;
-			}
-
-			it_prev = it;
+			int i = 0;
+			for( auto& b: _lBins )
+				b._startValue = vmin + i++ * _step;
 		}
-	}
-};
 
+		const auto begin() const { return _lBins.begin(); }
+		const auto end()   const { return _lBins.end();   }
+		auto begin() { return _lBins.begin(); }
+		auto end()   { return _lBins.end();   }
+
+		size_t nbBins() const { return _lBins.size(); }
+
+		void print( std::ostream& ) const;
+		PairAtvalClass getPoint(size_t idx) const
+		{
+			assert( p_src );
+			return p_src->at(idx);
+		}
+		bool splitBin( decltype( _lBins.begin() ) );
+		void assignToBin( const PairAtvalClass& pac, size_t idx );
+};
 
 //---------------------------------------------------------------------
 void
+MyHistogram::assignToBin( const PairAtvalClass& pac, size_t idx )
+{
+	bool match = true;
+	auto it_prev = _lBins.begin();
+	COUT << "searching bin for val=" << pac.first << '\n';
+	for( auto it=_lBins.begin(); it!=_lBins.end() && match; it++ )    // iterate the bins, except the last one
+	{
+		match = false;
+		auto& bin = *it;
+//		COUT << "trying thres "<< bin._startValue << '\n';
+		if( pac.first >= bin._startValue )
+		{
+			match = true;
+//			std::cout << "higher !\n";
+		}
+
+		if( match == false )
+		{
+//			COUT << "FOUND!\n";
+			auto& bin_prev = *it_prev;
+			bin_prev._vIdxPt.push_back( idx );
+			bin_prev._mClassCounter[pac.second]++;
+		}
+		else
+		{
+			if( std::next(it)==_lBins.end() )  // for values going in last bin
+			{
+//				COUT << "FOUND LAST!\n";
+				bin._vIdxPt.push_back( idx );
+				bin._mClassCounter[pac.second]++;
+			}
+		}
+		it_prev = it;
+	}
+}
+
+//---------------------------------------------------------------------
+void
+MyHistogram::print( std::ostream& f ) const
+{
+	f << "HISTOGRAM, nb bins=" << nbBins()
+		<< " v0=" << _vmin << " step=" << _step
+		<< '\n';
+	size_t i=0;
+	for( const auto& bin: _lBins )
+	{
+		f << "bin " << i++ << " nb pts=" << bin.size() << " nb classes=" << bin.nbClasses() << '\n';
+	}
+}
+//---------------------------------------------------------------------
+bool
 MyHistogram::splitBin( decltype( _lBins.begin() ) it )
 {
-	auto& bin = *it;
-	auto& it_next = std::next(it);
+	bool retval = false;
+	auto& bin = *it;                // current bin
+	auto it_next = std::next(it);  // next one (will insert before this one)
 	cout << "-start split bin at " << bin._startValue << '\n';
 	if( bin.isSplittable() )
 	{
@@ -136,18 +166,43 @@ MyHistogram::splitBin( decltype( _lBins.begin() ) it )
 		auto v1 = bin._startValue;
 		auto v2 = v1 + _step;
 		auto start2 = (v1+v2) / 2.;
-		MyBin newBin;
-		for( const auto id: bin._vIdxPt )
-		{
-			auto pt = getPoint(id);
-			if( pt.first >= start2 )
-		}
+		cout << "old thres: " << v1 << " - " << v2 << '\n';
+		cout << "new thres: " << v1 << " - " << start2 << " - " << v2 << '\n';
 
-		_lBins.insert( newBin );
+		MyBin newBin;
+		newBin._startValue = start2;
+
+		std::vector<size_t> newvec;  // new vector of indexes for the current bin
+		newvec.reserve( bin.size() );
+		bin._mClassCounter.clear();     // reset class counter
+
+		for( const auto idx: bin._vIdxPt )  // parse the points
+		{
+			COUT << "checling point " << idx << '\n';
+			auto pt = getPoint(idx);        // and distribute them in
+			if( pt.first >= start2 )           // the two bins
+			{
+				newBin._mClassCounter[ pt.second ]++;
+				newBin._vIdxPt.push_back( idx );
+			}
+			else
+			{
+				newvec.push_back( idx );
+				bin._mClassCounter[ pt.second ]++;
+			}
+		}
+		bin._vIdxPt = newvec;        // copy new vector of indexes for current bin
+		_lBins.insert( it_next, newBin );
+
+		auto b1 = splitBin( it );
+		auto b2 = splitBin( std::next(it) );
+
+		retval = true;
 	}
 	else
 		cout << " -NOT splittable!\n";
 
+	return retval;
 }
 
 //---------------------------------------------------------------------
@@ -194,77 +249,51 @@ getThresholds( const std::vector<PairAtvalClass>& v_pac, int nbBins )
 {
 	auto histo = buildHistogram( v_pac, nbBins );
 
-	cout << "Start splitting\n";
-	for( auto it=histo.start(); it!=histo.end(); it++ )
+	histo.print( std::cout );
+	cout << "Start splitting, nb bins=" << histo.nbBins();
+
+	for( auto it=histo.begin(); it!=histo.end(); it++ )
+
+	bool splitOccured = false;
+	int iter1 = 0;
+
+	do
 	{
-		histo.splitBin( it );
+		COUT << "iter1 " << iter1++ << '\n';
+		int iter2 = 0;
+		auto it = histo.begin();
+		do
+		{
+			COUT << "iter2 " << iter2++ << '\n';
+			splitOccured = histo.splitBin( it );
+			it = std::next(it);
+		}
+		while( !splitOccured && it != std::end(it); );
 	}
+	while( splitOccured );
+
 	std::vector<float> ret;
 	return ret;
 }
 
 int main()
 {
-	std::cout << "Hi!\n";
-
-	std::vector<PairAtvalClass> vpac;
-	vpac.push_back( std::make_pair( 0.5, ClassVal(1) ) );
+	auto c1 = ClassVal(1);
+	auto c2 = ClassVal(2);
+	auto c3 = ClassVal(3);
+	std::vector<PairAtvalClass> vpac{
+		{ 0., c1 },
+		{ 0.5, c1 },
+		{ 0.6, c1 },
+		{ 0.7, c2 },
+		{ 0.8, c2 },
+		{ 3,   c2 },
+	};
+/*	vpac.push_back( std::make_pair( 0., ClassVal(1) ) );
 	vpac.push_back( std::make_pair( 0.6, ClassVal(1) ) );
-	vpac.push_back( std::make_pair(   3, ClassVal(2) ) );
+	vpac.push_back( std::make_pair( 0.7, ClassVal(1) ) );
+	vpac.push_back( std::make_pair(   3, ClassVal(2) ) );*/
 	auto vThres = getThresholds( vpac, 3 );
 	std::cout << "Done !\n";
 }
-
-
-
-#if 0
-/*	struct my_axis {
-		axis::index_type index(const std::tuple<double, double>& point) const
-		{
-      const auto x = std::get<0>(point);
-      const auto y = std::get<1>(point);
-      return x * x + y * y <= 1.0;
-    }
-*/
-	auto nbBins = 10;
-	auto h = boost::histogram::make_histogram(
-		boost::histogram::axis::regular<>(
-			nbBins,
-			0., 10.
-		)
-	);
-
-	h( 5.3 );
-	h( 5.3 );
-	h( 6.3 );
-
-	cout << "bins:"
-		<< "\n0=" << h.at(0)
-		<< "\n5=" << h.at(5)
-		<< "\n6=" << h.at(6)
-		<< '\n';
-#endif
-
-#if 0
-	cout << "BOOST_VERSION=" << BOOST_VERSION << '\n';
-/*	struct MyType: accumulators::count<>
-	{
-// what is missing here ?
-		std::map<int,size_t> counter;
-	};
-
-	const auto axis = axis::regular<>(3, 0.0, 1.0);
-	auto h = make_histogram_with(dense_storage<MyType>(), axis);
-
-	h(0.15, sample(2));
-	h(0.14, sample(2));
-	h(0.16, sample(3));
-
-	cout
-		<< "bin 0: c2=" << h.at(0)->counter[2]   // should print 2
-		<< "bin 0: c3=" << h.at(0)->counter[3]   // should print 1
-		<< '\n';
-		*/
-#endif
-
 

1bf6e8e80025457c95aad51712f65be6b2991663
skramm <sebastien.kramm@univ-rouen.fr>
2021-03-23 16:49:36 +0100

    added sample data

---
+++ run_demo.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set +x
+app=build/bin/dectree
+
+if [ "$1" == "" ]
+then
+	echo "missing arg"
+	exit
+fi
+
+echo "arg=$1"
+
+case $1 in
+a)
+$app sample_data/iris.data -sep ','
+;;
+
+b)
+$app sample_data/winequality-white.csv -sep ';'
+;;
+
+c)
+$app sample_data/dummy_1.dat
+;;
+
+d)
+$app sample_data/dummy_2.dat
+;;
+
+e)
+$app sample_data/wine.data -sep ',' -cf
+;;
+
+f)
+$app sample_data/balance-scale.csv -fl -sep ',' $2 $3 $4 $5
+;;
+
+esac
+
